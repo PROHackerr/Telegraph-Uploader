4313f0bc0c409ee6260ff013e964218b7bba9d52
ÁÑÑÍHÌLÅTØR SPÄRK <tanmoyomg7@gmail.com>
2021-02-19 20:27:51 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -85,19 +85,17 @@ async def home(client, message):
         InlineKeyboardButton('Close', callback_data='close')
     ],
     [
-        InlineKeyboardButton('Our Channel', url='http://telegram.me/indusbots'),
-        InlineKeyboardButton('Source Code', url='https://github.com/benchamxd/Telegraph-Uploader')
+        InlineKeyboardButton('Our Channel', url='https://t.me/memerschatgroup'),
+        InlineKeyboardButton('Source Code', url='https://t.me/memerschatgroup')
     ]]
   reply_markup = InlineKeyboardMarkup(buttons)
   await Tgraph.send_message(
         chat_id=message.chat.id,
         text="""<b>Hey there,
-        
-im a telegraph Uploader That Can Upload Photo, Video And Gif
-        
-Simply send me photo, video or gif to upload to Telegra.ph
-        
-Made With Love By @indusBots</b>""",
+I'm a telegraph Uploader That Can Upload Photo, Video And Gif
+Simply Just Send Me A Video/ gif/ photo Upto 5mb to upload to Telegra.ph
+Deployed by @annihilatorrrr      
+from @indusBots</b>""",
         reply_markup=reply_markup,
         parse_mode="html",
         reply_to_message_id=message.message_id
@@ -110,14 +108,14 @@ async def help(client, message):
         InlineKeyboardButton('Close', callback_data='close')
     ],
     [
-        InlineKeyboardButton('Our Channel', url='http://telegram.me/indusbots')
+        InlineKeyboardButton('Our Channel', url='http://t.me/memerschatgroup')
     ]]
   reply_markup = InlineKeyboardMarkup(buttons)
   await Tgraph.send_message(
         chat_id=message.chat.id,
         text="""There Is Nothung To KnowMore,
         
-Just Send Me A Video/gif/photo Upto 5mb.
+Just Send Me A Video/gif/photo Upto 5mb only.
 
 i'll upload ut to telegra.ph and give you the direct link""",
         reply_markup=reply_markup,
